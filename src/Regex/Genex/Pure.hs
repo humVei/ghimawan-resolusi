04d0cc3b300d69c1c4cb3db7e65ced7280aed8b6
humVei <87869892+humVei@users.noreply.github.com>
2022-11-27 17:35:33 +0000

    adding source files

--- src/Regex/Genex/Pure.hs
+++ src/Regex/Genex/Pure.hs
@@ -2,4 +2,6 @@
 module Regex.Genex.Pure (genexPure) where
 import qualified Data.Text as T
 import qualified Data.IntSet as IntSet
-import qualified Data.Se
+import qualified Data.Set as Set
+import Data.List (intersect, (\\))
+import Control.Mon
