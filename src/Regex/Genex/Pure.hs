de6b096ff6f93b52dc4ada6192c363724dfae26b
humVei <87869892+humVei@users.noreply.github.com>
2022-11-27 21:00:35 +0000

    Increased version to 1.0.4-SNAPSHOT

--- src/Regex/Genex/Pure.hs
+++ src/Regex/Genex/Pure.hs
@@ -4,4 +4,6 @@ import qualified Data.Text as T
 import qualified Data.IntSet as IntSet
 import qualified Data.Set as Set
 import Data.List (intersect, (\\))
-import Control.Mon
+import Control.Monad
+import Control.Monad.Stream
+import qualified Control.Monad.
